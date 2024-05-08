a51e2819a92fbbd44ae62b0859dc73a32fd51893
rileysurratt <rileysurratt13@gmail.com>
2024-05-08 17:26:02 -0400

    finished setting up, figure out how to target and style next

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
+console.log("puppyList:", featuredPup);
 
   return (
     <>
-      <div>
-
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+        {
+          featPupId && (
+            // use bootstrap components to style???
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+          )}
       </div>
     </>
   )
